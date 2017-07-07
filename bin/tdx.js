f9a08459949d84b12f146c1cc0623497a90af3e2
kuangcaibao <whkcb@163.com>
2017-07-07 19:17:44 +0800

    help

--- bin/tdx.js
+++ bin/tdx.js
@@ -4,13 +4,22 @@ const program = require("commander");
 
 program
   .version(require("../package.json").version)
-  .usage("[options] [project-name]")
+  .usage("[options] <project-name>")
   .option("-c, --create", "create a project")
   .parse(process.argv);
 
 if(program.create) {
   create();
 }
+
+/**
+ * help 
+ */
+function help() {
+  console.log(program.args.length);
+  // if(program.args.length)
+}
+help();
 
 // 创建项目逻辑
 function create() {
