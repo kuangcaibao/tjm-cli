54ca8840aa4bd86b81e93cfb0098171e7e2f5fe1
kuangcaibao <whkcb@163.com>
2017-07-10 09:34:03 +0800

    update

--- update.sh
+++ update.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 echo "--- publish package---"
-git add . &&
-git commit -m "update" &&
-npm version patch &&
+git add .
+git commit -m "update"
+npm version patch
+npm publish
 
 echo "--- update package ---"
-npm uninstall -g tdx-cli-test &&
+npm uninstall -g tdx-cli-test
 npm install -g tdx-cli-test
